a1b97d01a9c436dc6c19bc83d0aad83f64f9d44c
SSB4455 <741086@gmail.com>
2018-11-21 17:11:36 +0800

    Unity3D用LearnCloud实现排行榜 part1

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,55 @@
+﻿using LeanCloud;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public Text outputText;
+	public Button submitButton;
+	public InputField intputField;
+
+
+
+	// Use this for initialization
+	void Start()
+	{
+		new AVQuery<AVObject>("GameScore").FindAsync().ContinueWith(t2 =>
+		{
+			string scoresStr = "";
+			foreach (AVObject avobj in t2.Result)
+			{
+				Debug.Log(avobj["playerName"] + " " + avobj["score"] + " " + avobj.CreatedAt);
+				scoresStr += avobj.CreatedAt.Value.ToString("yyyy-MM-dd HH:mm:ss") + " |\t" + avobj["score"] + "\t|\t" + avobj["playerName"] + "\n";
+			}
+			outputText.text = scoresStr;
+		});
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void SubmitScore()
+	{
+		AVObject gameScore = new AVObject("GameScore");
+		gameScore["playerName"] = "player1";
+		gameScore["score"] = int.Parse(intputField.text);
+		intputField.text = int.Parse(intputField.text) + 1 + "";
+		Task saveTask = gameScore.SaveAsync().ContinueWith(t =>
+		{
+			new AVQuery<AVObject>("GameScore").FindAsync().ContinueWith(t2 =>
+			{
+				string scoresStr = "";
+				foreach(AVObject avobj in t2.Result )
+				{
+					Debug.Log(avobj["playerName"] + " " + avobj["score"] + " " + avobj.CreatedAt);
+					scoresStr += avobj.CreatedAt.Value.ToString("yyyy-MM-dd HH:mm:ss") + " |\t" + avobj["score"] + "\t|\t" + avobj["playerName"] + "\n";
+				}
+				outputText.text = scoresStr;
+			});
+		});
+	}
+}
