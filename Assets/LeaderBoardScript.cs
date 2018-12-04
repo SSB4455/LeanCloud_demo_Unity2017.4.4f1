4d003528f0e00e6ad289ffc1642b7aeac6f78e0c
SSB4455 <741086@gmail.com>
2018-12-04 17:41:43 +0800

    Unity3D用LeanCloud实现排行榜3

--- Assets/LeaderBoardScript.cs
+++ Assets/LeaderBoardScript.cs
@@ -14,7 +14,20 @@ public class LeaderBoardScript : MonoBehaviour
 	// Use this for initialization
 	void Start()
 	{
+		LoginOrSignin();
 		UpdateLeaderBoardText();
+	}
+
+	private void LoginOrSignin()
+	{
+		if (AVUser.CurrentUser == null)
+		{
+			AVUser user = new AVUser();
+			user.Username = "player1";
+			user.Password = "player1";
+			user.Email = "player1@player1.com";
+			user.SignUpAsync();
+		}
 	}
 
 	// Update is called once per frame
@@ -40,7 +53,12 @@ public class LeaderBoardScript : MonoBehaviour
 				leaderBoardScore = new AVObject("GameScore");
 				leaderBoardScore["playerName"] = "player1";
 				leaderBoardScore["score"] = int.Parse(intputField.text);
-				intputField.text = int.Parse(intputField.text) + 1 + "";
+				if (AVUser.CurrentUser != null)
+				{
+					leaderBoardScore.ACL = new AVACL();
+					leaderBoardScore.ACL.PublicReadAccess = true;
+					leaderBoardScore.ACL.SetWriteAccess(AVUser.CurrentUser, true);
+				}
 			}
 			if ((int)(leaderBoardScore["score"]) < int.Parse(intputField.text))
 			{
@@ -58,6 +76,10 @@ public class LeaderBoardScript : MonoBehaviour
 		new AVQuery<AVObject>("GameScore").OrderByDescending("score").FindAsync().ContinueWith(t =>
 		{
 			string scoresStr = "";
+			if (AVUser.CurrentUser != null)
+			{
+				scoresStr = "CurrentUser is \'" + AVUser.CurrentUser.Username + "\'\n";
+			}
 			foreach (AVObject avobj in t.Result)
 			{
 				Debug.Log(avobj["playerName"] + " " + avobj["score"] + " " + avobj.UpdatedAt);
